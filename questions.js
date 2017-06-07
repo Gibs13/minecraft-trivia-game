1712365973963d06a880b6ada996a74d06d095e0
Gibs13 <Gsgwlol@gmail.com>
2017-06-07 21:33:08 +0200

    modified questions

--- questions.js
+++ questions.js
@@ -3,16 +3,38 @@
 module.exports = [
 	{	"What is a creeper ?" : [
 		"A monster.",
-		"An item.",
+		"A type of sand.",
 		"A flower.",
-		"A biome."
+		"A biome.",
+		"A NPC.",
+		"Some shadows in the night.",
+		"A mineral."
 		]
 	},
-	{	"How do you get wood ?" : [
-		"Cutting trees.",
-		"Melting iron.",
-		"Using tnt on walls.",
-		"Combining an axe with a branch."
+	{	"What's the condition to find diamond ore ?" : [
+		"Being deep in the ground.",
+		"Being at least level 30.",
+		"You can't find diamond ore.",
+		"Trading with a villager.",
+		"Finding a dungeon.",
+		"Having a pickaxe in your inventory.",
+		"Killing the ender dragon."
+		]
+	},
+	{	"Who is the creator of minecraft ?" : [
+		"Markus Persson.",
+		"Jens Bergensten.",
+		"Microsoft.",
+		"Jakob Porsér.",
+		"Hypixel."
+		]
+	},
+	{	"How do you craft a beacon ?" : [
+		"Obsidian, glass and a star.",
+		"Iron, an endereye and a dragon egg.",
+		"Tnt, netherrack and lava.",
+		"Wool, glowing stone and a black carpet.",
+		"Diamond blocks, black clay and a bottle"
 		]
 	}
 ]
